505e707c753f0ca7f44aad7bf512c088138a1c63
Kanish1411 <kanishkark1411@gmail.com>
2024-07-22 20:37:48 +0530

    abc

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template, jsonify, request,make_response
 from io import BytesIO
-import pyaudio
-import numpy as np
-import wave
-import os
-import json
 import asyncio
 from deepgram import Deepgram
 from openai import OpenAI
-import time
 import google.generativeai as genai
 from gtts import gTTS
 
@@ -16,13 +10,6 @@ from gtts import gTTS
 app = Flask(__name__)
 
 # Constants
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-SAMPLE_RATE = 16000
-CHUNK = 1024
-MAX_RECORDING_TIME = 20  # seconds
-SILENCE_THRESHOLD = 100  # adjust as needed
-SILENCE_DURATION = 3  # seconds
 
 dg = Deepgram("fe9cb1acb73d32204b6b8d3ed2e07175fd2e765f")
 client = OpenAI(api_key="sk-None-BT7vYnLkVjjG571rNam4T3BlbkFJXuqsEcQfRYj15xYPIFIO",)
@@ -49,61 +36,60 @@ options = {
     "tier": "enhanced"
 }
 
-async def speechtosudio():
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=SAMPLE_RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-    print(f"Recording for up to {MAX_RECORDING_TIME} seconds or until silence is detected...")
-    frames = []
-    start_time = time.time()
-    silence_start_time = None
-    while True:
-        data = stream.read(CHUNK)
-        frames.append(data)
-        audio_data = np.frombuffer(data, dtype=np.int16)
-        rms = np.sqrt(np.mean(np.square(audio_data)))
+# async def speechtosudio():
+#     p = pyaudio.PyAudio()
+#     stream = p.open(format=FORMAT,
+#                     channels=CHANNELS,
+#                     rate=SAMPLE_RATE,
+#                     input=True,
+#                     frames_per_buffer=CHUNK)
+#     print(f"Recording for up to {MAX_RECORDING_TIME} seconds or until silence is detected...")
+#     frames = []
+#     start_time = time.time()
+#     silence_start_time = None
+#     while True:
+#         data = stream.read(CHUNK)
+#         frames.append(data)
+#         audio_data = np.frombuffer(data, dtype=np.int16)
+#         rms = np.sqrt(np.mean(np.square(audio_data)))
         
-        if rms < SILENCE_THRESHOLD:
-            if silence_start_time is None:
-                silence_start_time = time.time()
-            elif time.time() - silence_start_time > SILENCE_DURATION:
-                print("Silence detected. Stopping recording.")
-                break
-        else:
-            silence_start_time = None  
+#         if rms < SILENCE_THRESHOLD:
+#             if silence_start_time is None:
+#                 silence_start_time = time.time()
+#             elif time.time() - silence_start_time > SILENCE_DURATION:
+#                 print("Silence detected. Stopping recording.")
+#                 break
+#         else:
+#             silence_start_time = None  
         
-        if time.time() - start_time > MAX_RECORDING_TIME:
-            print("Maximum recording time reached. Stopping recording.")
-            break
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    output_file = "audio/output.wav"
-    wf = wave.open(output_file, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(SAMPLE_RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+#         if time.time() - start_time > MAX_RECORDING_TIME:
+#             print("Maximum recording time reached. Stopping recording.")
+#             break
+#     stream.stop_stream()
+#     stream.close()
+#     p.terminate()
+#     output_file = "audio/output.wav"
+#     wf = wave.open(output_file, 'wb')
+#     wf.setnchannels(CHANNELS)
+#     wf.setsampwidth(p.get_sample_size(FORMAT))
+#     wf.setframerate(SAMPLE_RATE)
+#     wf.writeframes(b''.join(frames))
+#     wf.close()
 
-async def Audiototext():
-    audio_files = os.listdir("./audio")
-    for audio_file in audio_files:
-        audio_path = f"audio/{audio_file}"
-        with open(audio_path, "rb") as file:
-            src = {
-                "buffer": file,
-                "mimetype": "audio/wav"  # Correct MIME type for WAV files
-            }
-            try:
-                res = await dg.transcription.prerecorded(src, options)
-                return res["results"]["channels"][0]["alternatives"][0]["transcript"] 
-            except Exception as e:
-                print(f"Error processing {audio_file}: {e}")
-        
+# async def Audiototext():
+#     audio_files = os.listdir("./audio")
+#     for audio_file in audio_files:
+#         audio_path = f"audio/{audio_file}"
+#         with open(audio_path, "rb") as file:
+#             src = {
+#                 "buffer": file,
+#                 "mimetype": "audio/wav"  # Correct MIME type for WAV files
+#             }
+#             try:
+#                 res = await dg.transcription.prerecorded(src, options)
+#                 return res["results"]["channels"][0]["alternatives"][0]["transcript"] 
+#             except Exception as e:
+#                 print(f"Error processing {audio_file}: {e}")
 
 async def LLm(prompt):
     # resp = client.chat.completions.create(
